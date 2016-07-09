1ac9a08bbab36dc91060d5b0b86e3dbc622b9b80
Santiago0076 <santiago007roman@hotmail.com>
2016-07-09 16:01:44 -0400

    Update exercises.py
    "update files and add the last part"

--- assignment_5/exercises.py
+++ assignment_5/exercises.py
@@ -16,44 +16,43 @@ def to_celsius(degrees_fahrenheit):
 
 to_fahrenheit(0)
 
-print(to_fahrenheit(0))
-print(to_fahrenheit(50))
-print(to_fahrenheit(75))
-print(to_fahrenheit(100))
-
-print(to_celsius(0))
-print(to_celsius(50))
-print(to_celsius(75))
-print(to_celsius(100))
-
-
-
-
+gravity = 9.8
 
 def get_fall_time(height):
-    height = float("Enter the height: ") #meters
-    get_fall_time = math.sqrt(2) * height * gravity #calculate the velocity
-    gravity = 9.8
-    
-    return height 
-    print('get_fall_time ')
+    # gravity isn't going to change, units in m/(s^2)
+
+
+    time_elapsed = math.sqrt((2 * height) / gravity)
+
+    # replace with logic of above equation
+    return time_elapsed
+
+def height(height):
+    height = (1/2) * gravity * time * time
+
+def time(time):
+    time = math.sqrt((2 * height) / gravity)
+    return time
+
+print(get_fall_time(10))
 
 
 def isVulnerable(tower_height, tower_x, tower_y, target_x, target_y):
     muzzle_velocity = 300
 
     # update this line to calculate time_in_air using get_fall_time() function
-    time_in_air = get_fall_time
+    time_in_air = get_fall_time(tower_height)
 
     tower_range = time_in_air * muzzle_velocity
 
     delta_x = tower_x - target_x  # difference between tower_x and target_x
     delta_y = tower_y - target_y  # difference between tower_y and target_y
 
-    separation = None  # the x and y deltas form a triangle, find the hypotenuse
+    separation = math.sqrt(delta_x**2 + delta_y**2)  # the x and y deltas form a triangle, find the hypotenuse
     if separation < tower_range:
         print("The target is closer than the tower range, what should we return?")
         return None
     else:
         print("The target is further than the tower range, what should we return?")
-        return None   
+        return None
+    isVulnerable(10, 3, 2, 1, 1)
